64db61bb489c15460950399724c4e890be7aff9c
Administrator <Administrator@10.39.72.250>
2019-01-23 09:45:22 +0800

    first commit

---
+++ src/com/sunyard/pojo/Flow.java
@@ -0,0 +1,28 @@
+package com.sunyard.pojo;
+
+public class Flow {
+	private String flow;
+	private String pricture;
+	private Integer id;
+	public String getFlow() {
+		return flow;
+	}
+	public void setFlow(String flow) {
+		this.flow = flow;
+	}
+	public String getPricture() {
+		return pricture;
+	}
+	public void setPricture(String pricture) {
+		this.pricture = pricture;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void showMessage(){
+		System.out.println("aaaaaaa");
+	}
+}	
